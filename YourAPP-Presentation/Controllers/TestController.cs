30c61fb6dad83a902eb6949dfd30eb79d6eec9db
saif lotfy <saiflotfy26@gmail.com>
2026-04-22 07:38:29 +0200

    Feat TestController : Adding a test Controller

---
+++ YourAPP-Presentation/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace YourAPP_Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello, World!";
+        }
+    }
+}
